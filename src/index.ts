fa1f2094c01c800108736af84af687ed59e73008
yotarotsukada <yotarotsukada@gmail.com>
2022-09-17 23:45:33 +0900

    fix: reset using cluster

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
-import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
-import { setupMaster, setupWorker } from '@socket.io/sticky';
-import cluster from 'cluster';
-import cors, { CorsOptions } from 'cors';
 import 'dotenv/config';
+import cors, { CorsOptions } from 'cors';
 import express, { Request, Response } from 'express';
 import { createServer } from 'http';
-import os from 'os';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 const app = express();
 const httpServer = createServer(app);
@@ -14,13 +10,20 @@ const httpServer = createServer(app);
 const CLIENT_URL = process.env.DEV
   ? 'http://localhost:5173'
   : 'https://sss-games.vercel.app';
-const corsOptions: CorsOptions = {
+const corsOption: CorsOptions = {
   origin: CLIENT_URL,
-  methods: ['GET', 'POST'],
   credentials: true,
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOption));
+
+const io = new Server(httpServer, { cors: corsOption });
+io.on('connection', (socket: Socket) => {
+  console.log(`socket client connected at ${new Date()}`);
+  socket.on('message', (message) => {
+    io.emit('message', `${message} (${new Date().toISOString()})`);
+  });
+});
 
 app.get('/', (req: Request, res: Response) => {
   console.log(`"/" requested at ${new Date()}`);
@@ -31,60 +34,9 @@ app.get('/user', (req: Request, res: Response) => {
   setTimeout(() => res.send({ name: 'Yotaro' }), 1000);
 });
 
-if (cluster.isPrimary) {
-  console.log(`Master ${process.pid} is running`);
-
-  // setup sticky sessions
-  setupMaster(httpServer, {
-    loadBalancingMethod: 'least-connection',
-  });
-
-  // setup connections between the workers
-  setupPrimary();
-
-  // needed for packets containing buffers (you can ignore it if you only send plaintext objects)
-  // Node.js < 16.0.0
-  // cluster.setupMaster({
-  //   serialization: 'advanced',
-  // });
-  // Node.js > 16.0.0
-  cluster.setupPrimary({
-    // @ts-ignore
-    serialization: 'advanced',
-  });
-
-  const port = process.env.PORT || 8080;
-  httpServer.listen(port, () => {
-    if (process.env.DEV) {
-      console.log(`[server]: Server is running at http://localhost:${port}`);
-    }
-  });
-  const numCPUs = os.cpus().length;
-
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
+const port = process.env.PORT || 8080;
+httpServer.listen(port, () => {
+  if (process.env.DEV) {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
   }
-
-  cluster.on('exit', (worker: { process: { pid: any } }) => {
-    console.log(`Worker ${worker.process.pid} died`);
-    cluster.fork();
-  });
-} else {
-  console.log(`Worker ${process.pid} started`);
-
-  const httpServer = createServer();
-  const io = new Server(httpServer, { cors: corsOptions });
-
-  // use the cluster adapter
-  io.adapter(createAdapter());
-
-  // setup connection with the primary process
-  setupWorker(io);
-
-  io.on('connection', (socket) => {
-    console.log(`socket client connected at ${new Date()}`);
-    socket.on('message', (message) => {
-      io.emit('message', `${message} (${new Date().toISOString()})`);
-    });
-  });
-}
+});
